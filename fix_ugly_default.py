7c4bb98216b64ebc97e0c6befc716d895678165e
Brian Brookman <bcbrookman@users.noreply.github.com>
2020-07-17 23:21:32 -0700

    Use continous while instead of cron

--- fix_ugly_default.py
+++ fix_ugly_default.py
@@ -1,17 +1,18 @@
 # When powering on after a power failure or light switch on/off event, bulbs occasionally do not follow the power-on
-# rules defined in the Hue app and default to a warm yellow. This script can be scheduled in a cron job to run every
-# minute to change the color of the bulbs to the "Energize" scene.
+# rules defined in the Hue app and default to a warm yellow. This script checks every 30 seconds and changes the color
+# of the bulbs to the "Energize" scene.
 
 import json
 import os
 import requests
+import time
 
 username = os.environ['HUE_USERNAME']
 hub = os.environ['HUE_HUB']
 api_url = f"http://{hub}/api/{username}/lights"
 
 headers = {"Content-Type": "application/json"}
-lights_dict = requests.get(api_url).json()
+
 
 ugly_default_xy = {
     "on": True,
@@ -40,17 +41,23 @@ energize = {
     ]
 }
 
-for light, info in lights_dict.items():
-    if info["state"]["on"] and info["state"]["reachable"]:
 
-        if info["state"]["colormode"] == "xy" and "xy" in info["state"]:
-            if info["state"]["xy"][0] == ugly_default_xy["xy"][0] \
-                    and info["state"]["xy"][1] == ugly_default_xy["xy"][1] \
-                    and info["state"]["bri"] == ugly_default_xy["bri"]:
-                requests.put(f"{api_url}/{light}/state", data=json.dumps(energize), headers=headers)
+while True:
+    lights_dict = requests.get(api_url).json()
 
-        elif info["state"]["colormode"] == "ct" and "xy" in info["state"]:
-            if info["state"]["xy"][0] == ugly_default_ct["xy"][0] \
-                    and info["state"]["xy"][1] == ugly_default_ct["xy"][1] \
-                    and info["state"]["bri"] == ugly_default_ct["bri"]:
-                requests.put(f"{api_url}/{light}/state", data=json.dumps(energize), headers=headers)
+    for light, info in lights_dict.items():
+        if info["state"]["on"] and info["state"]["reachable"]:
+
+            if info["state"]["colormode"] == "xy" and "xy" in info["state"]:
+                if info["state"]["xy"][0] == ugly_default_xy["xy"][0] \
+                        and info["state"]["xy"][1] == ugly_default_xy["xy"][1] \
+                        and info["state"]["bri"] == ugly_default_xy["bri"]:
+                    requests.put(f"{api_url}/{light}/state", data=json.dumps(energize), headers=headers)
+
+            elif info["state"]["colormode"] == "ct" and "xy" in info["state"]:
+                if info["state"]["xy"][0] == ugly_default_ct["xy"][0] \
+                        and info["state"]["xy"][1] == ugly_default_ct["xy"][1] \
+                        and info["state"]["bri"] == ugly_default_ct["bri"]:
+                    requests.put(f"{api_url}/{light}/state", data=json.dumps(energize), headers=headers)
+
+    time.sleep(30)
